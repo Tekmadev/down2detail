59c7f90103e9cd8ed85c0fd4c561d08dead16d45
Hamza Ekram <hamza.ekram786@gmail.com>
2025-04-21 17:42:20 -0400

    WEB-7 <la page service finit les page se relier et il y a un page juste service aussi>

--- data/services.ts
+++ data/services.ts
@@ -6,7 +6,7 @@ export type Service = {
     shortDescription: string;
     features: string[];
     benefits: string[];
-    price: number;
+    price: string;
     currency: string;
     image: string;
     description: string;
@@ -27,7 +27,7 @@ export type Service = {
       description:
         "Experience a high-quality interior detailing service with our specialized stain extraction techniques. We’ll target and remove stains from your carpets, fabric seats, and other surfaces.",
       imageShowcase: "/images/services-detailing/intext.jpg",
-      price: 129.99,
+      price: "129.99",
       currency: "CAD",
       image: "/images/services/full-detailing.jpg"
     },
@@ -42,7 +42,7 @@ export type Service = {
       description:
         "Enhance the appearance of your vehicle with our Deep Clean paint service, which eliminates stubborn contaminants like airborne compounds, brake dust, and rail dust while also boosting shine of the car.",
       imageShowcase: "/images/services-detailing/gloss.jpg",
-      price: 369.99,
+      price: "369.99",
       currency: "CAD",
       image: "/images/services/gloss-enhancer.jpg"
     },
@@ -57,7 +57,7 @@ export type Service = {
       description:
         "Revitalize your car’s engine bay with our expert cleaning service, eliminating any build-up of dirt, grease, and grime for a spotless and well-maintained appearance.",
       imageShowcase: "/images/services-detailing/Cleaning-engine.webp",
-      price: 109.99,
+      price: "109.99",
       currency: "CAD",
       image: "/images/services/engine-bay.jpg"
     },
@@ -73,11 +73,25 @@ export type Service = {
       features: ["Paint-safe hand wash", "Clay bar treatment", "Premium wax finish"],
       benefits: ["Protects paint", "Boosts shine", "Enhances resale value"],
       description:
-        "Revitalize your vehicle’s exterior with our meticulous detailing services, including paint correction, waxing, and professional sealant application for lasting shine and protection.",
+        "Deep clean paint, remove embedded contaminants and protective wax application to restore your vehicle's shine and shield it from environmental elements, ensuring a lasting glossy finish.",
       imageShowcase: "/images/services-detailing/wash.jpg",
-      price: 119.99,
+      price: "119.99",
       currency: "CAD",
       image: "/images/services/wash-wax.jpg",
+      faqs:[
+        {
+          question: "What is included in the Exterior Wash & Wax service?",
+          answer : "We provide a paint-safe hand wash, clay bar treatment, and a premium wax finish to protect your paint and enhance shine."
+        },
+        {
+          question: "How long does a typical detailing service take?",
+          answer : "Most services take between 1 to 4 hours, depending on the package and condition of the vehicle."
+        },
+        {
+          question: "Are your products safe for all vehicles?",
+          answer : "Yes! We use professional-grade, paint-safe and interior-safe products suitable for all vehicle types."
+        },
+      ]
     },
     {
       id: "interior-detailing",
@@ -88,11 +102,25 @@ export type Service = {
       features: ["Stain extraction", "Vacuuming", "Interior surface protection"],
       benefits: ["Removes odors", "Cleans deeply", "Protects interior materials"],
       description:
-        "Experience a high-quality interior detailing service with our specialized stain extraction techniques. We’ll target and remove stains from your carpets, fabric seats, and other surfaces.",
+        "Experience a high-quality interior detailing service with our specialized stain extraction techniques. We'll target and remove stains from your carpets, fabric seats, and any other surfaces, giving you a truly flawless and inviting cabin. Deep cleaning of all interior surfaces, followed by a meticulous vacuuming to eliminate dirt, and creating a fresh and pristine interior environment.",
       imageShowcase: "/images/services-detailing/interior.jpeg",
-      price: 149.99 - 279.99,
+      price: "149.99 - 279.99",
       currency: "CAD",
-      image: "/images/services/interior-detailing.jpg"
+      image: "/images/services/interior-detailing.jpg",
+      faqs:[
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Can I drop off my car early or late?",
+          answer : "Yes, we offer flexible drop-off and pickup times. Let us know ahead, and we’ll do our best to accommodate you."
+        },
+        {
+          question: "Is there a cancellation policy?",
+          answer : "Yes — we kindly ask for 24 hours' notice for cancellations or rescheduling, so we can manage bookings efficiently."
+        },
+      ]
     },
     {
       id: "interior-exterior-detailing",
@@ -103,11 +131,25 @@ export type Service = {
       features: ["Full interior + exterior", "Vacuum & shampoo", "Wax & sealant"],
       benefits: ["All-in-one service", "Restores vehicle condition", "Time-saving bundle"],
       description:
-        "Experience a high-quality interior detailing service with our specialized stain extraction techniques. We’ll target and remove stains from your carpets, fabric seats, and other surfaces.",
+        "Experience a high-quality interior detailing service with our specialized stain extraction techniques. Our interior and exterior detailing service rejuvenates your car's interior and exterior.",
       imageShowcase: "/images/services-detailing/intext.jpg",
-      price: 129.99,
+      price: "129.99",
       currency: "CAD",
-      image: "/images/services/full-detailing.jpg"
+      image: "/images/services/full-detailing.jpg",
+      faqs:[
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Can I drop off my car early or late?",
+          answer : "Yes, we offer flexible drop-off and pickup times. Let us know ahead, and we’ll do our best to accommodate you."
+        },
+        {
+          question: "Is there a cancellation policy?",
+          answer : "Yes — we kindly ask for 24 hours' notice for cancellations or rescheduling, so we can manage bookings efficiently."
+        },
+      ]
     },
     {
       id: "gloss-enhancer",
@@ -120,9 +162,23 @@ export type Service = {
       description:
         "Enhance the appearance of your vehicle with our Deep Clean paint service, which eliminates stubborn contaminants like airborne compounds, brake dust, and rail dust while also boosting shine of the car.",
       imageShowcase: "/images/services-detailing/gloss.jpg",
-      price: 369.99,
+      price: "369.99",
       currency: "CAD",
-      image: "/images/services/gloss-enhancer.jpg"
+      image: "/images/services/gloss-enhancer.jpg",
+      faqs:[
+        {
+          question: "Do I need to make an appointment?",
+          answer : "Yes, appointments help us ensure high-quality service and availability. You can book directly from the service pages."
+        },
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Can I drop off my car early or late?",
+          answer : "Yes, we offer flexible drop-off and pickup times. Let us know ahead, and we’ll do our best to accommodate you."
+        },
+      ]
     },
     {
       id: "one-step-polish",
@@ -135,9 +191,23 @@ export type Service = {
       description:
         "With our exceptional services, we can effectively eliminate 50%-70% of scratches and swirls, leaving your car with a flawless, glossy finish.",
       imageShowcase: "/images/services-detailing/One-ste.jpg",
-      price: 449.99 - 549.99,
+      price: "449.99 - 549.99",
       currency: "CAD",
-      image: "/images/services/one-step-polish.jpg"
+      image: "/images/services/one-step-polish.jpg",
+      faqs:[
+        {
+          question: "Are your products safe for all vehicles?",
+          answer : "Yes! We use professional-grade, paint-safe and interior-safe products suitable for all vehicle types."
+        },
+        {
+          question: "Do I need to make an appointment?",
+          answer : "Yes, appointments help us ensure high-quality service and availability. You can book directly from the service pages."
+        },
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+      ]
     },
     {
       id: "paint-correction",
@@ -150,9 +220,23 @@ export type Service = {
       description:
         "Elevate your vehicle’s appearance with our professional auto detailing techniques and high-quality products, eliminating up to 90% of imperfections on your car’s paint.",
       imageShowcase: "/images/services-detailing/paint.jpg",
-      price: 749.99 - 949.99,
+      price: "749.99 - 949.99",
       currency: "CAD",
-      image: "/images/services/paint-correction.jpg"
+      image: "/images/services/paint-correction.jpg",
+      faqs:[
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Can I drop off my car early or late?",
+          answer : "Yes, we offer flexible drop-off and pickup times. Let us know ahead, and we’ll do our best to accommodate you."
+        },
+        {
+          question: "Is there a cancellation policy?",
+          answer : "Yes — we kindly ask for 24 hours' notice for cancellations or rescheduling, so we can manage bookings efficiently."
+        },
+      ]
     },
     {
       id: "ceramic-coating",
@@ -165,9 +249,19 @@ export type Service = {
       description:
         "Experience the ultimate protection and enhancement for your car’s paint, wheels, windows, plastics, and leathers with our advanced ceramic coating. Lasts up to 1–4 years.",
       imageShowcase: "/images/services-detailing/ceramic.jpg",
-      price: 159.99 - 359.99,
+      price: "159.99 - 359.99",
       currency: "CAD",
-      image: "/images/services/ceramic-coating.jpg"
+      image: "/images/services/ceramic-coating.jpg",
+      faqs:[
+        {
+          question: "Are your products safe for all vehicles?",
+          answer : "Yes! We use professional-grade, paint-safe and interior-safe products suitable for all vehicle types."
+        },
+        {
+          question: "Do I need to make an appointment?",
+          answer : "Yes, appointments help us ensure high-quality service and availability. You can book directly from the service pages."
+        },
+      ]
     },
     {
       id: "engine-bay-cleaning",
@@ -180,9 +274,23 @@ export type Service = {
       description:
         "Revitalize your car’s engine bay with our expert cleaning service, eliminating any build-up of dirt, grease, and grime for a spotless and well-maintained appearance.",
       imageShowcase: "/images/services-detailing/Cleaning-engine.webp",
-      price: 109.99,
+      price: "109.99",
       currency: "CAD",
-      image: "/images/services/engine-bay.jpg"
+      image: "/images/services/engine-bay.jpg",
+      faqs:[
+        {
+          question: "Are your products safe for all vehicles?",
+          answer : "Yes! We use professional-grade, paint-safe and interior-safe products suitable for all vehicle types."
+        },
+        {
+          question: "Do I need to make an appointment?",
+          answer : "Yes, appointments help us ensure high-quality service and availability. You can book directly from the service pages."
+        },
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+      ]
     },
     {
       id: "headlight-restoration",
@@ -195,9 +303,23 @@ export type Service = {
       description:
         "Enhance visibility and aesthetics with our headlight restoration, addressing cloudiness and yellowing to improve both safety and the overall look of your vehicle.",
       imageShowcase: "/images/services-detailing/headlight.jpeg",
-      price: 129.99 - 259.99,
+      price: "129.99 - 259.99",
       currency: "CAD",
-      image: "/images/services/headlight-restoration.jpg"
+      image: "/images/services/headlight-restoration.jpg",
+      faqs:[
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Can I drop off my car early or late?",
+          answer : "Yes, we offer flexible drop-off and pickup times. Let us know ahead, and we’ll do our best to accommodate you."
+        },
+        {
+          question: "Is there a cancellation policy?",
+          answer : "Yes — we kindly ask for 24 hours' notice for cancellations or rescheduling, so we can manage bookings efficiently."
+        },
+      ]
     },
     {
       id: "headlight-tint",
@@ -208,11 +330,25 @@ export type Service = {
       features: ["UV protection", "Glare reduction", "Improves beam output"],
       benefits: ["Better night vision", "Saves replacement costs", "Increases safety"],
       description:
-        "Reduce glare and improve visibility with a headlight tint that lasts up to 5 years.",
+        "Keep your car protected with tint film. Our kits contain tinted taillights and headlights covers that protect your car or truck from the environment. It covers for cars and trucks are available in several shades and colors. Choose between different levels of tail light & headlight tint film that can help with scratches or other imperfections.",
       imageShowcase: "/images/services-detailing/tint.jpg",
-      price: 179.99,
+      price: "179.99",
       currency: "CAD",
-      image: "/images/services/headlight-tint.jpg"
+      image: "/images/services/headlight-tint.jpg",
+      faqs:[
+        {
+          question: "Do I need to make an appointment?",
+          answer : "Yes, appointments help us ensure high-quality service and availability. You can book directly from the service pages."
+        },
+        {
+          question: "Will detailing help with resale value?",
+          answer : "Yes! A well-detailed car looks newer and more appealing to buyers, often increasing resale value."
+        },
+        {
+          question: "Is there a cancellation policy?",
+          answer : "Yes — we kindly ask for 24 hours' notice for cancellations or rescheduling, so we can manage bookings efficiently."
+        },
+      ]
     },
     {
       id: "leather-seat-treatment",
@@ -223,11 +359,21 @@ export type Service = {
       features: ["Leather cleaner", "Premium conditioner", "UV protection"],
       benefits: ["Preserves softness", "Prevents aging", "Restores appearance"],
       description:
-        "Indulge in luxurious leather seat treatment, including thorough cleaning and conditioning to preserve your vehicle’s leather upholstery.",
+        "Experience the ultimate in leather seat care with our luxurious treatment, including meticulous cleaning and conditioning to preserve the elegance and durability of your vehicle's leather upholstery.",
       imageShowcase: "/images/services-detailing/leather.jpg",
-      price: 69.99,
+      price: "69.99",
       currency: "CAD",
-      image: "/images/services/leather-treatment.jpg"
+      image: "/images/services/leather-treatment.jpg",
+      faqs:[
+        {
+          question: "How long does a typical detailing service take?",
+          answer : "Most services take between 1 to 4 hours, depending on the package and condition of the vehicle."
+        },
+        {
+          question: "Are your products safe for all vehicles?",
+          answer : "Yes! We use professional-grade, paint-safe and interior-safe products suitable for all vehicle types."
+        },
+      ]
     }
   ];
   
