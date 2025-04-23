15a93d481ff726f198ba8630db8b0cfb4fba48d3
shajeed <shajeed@live.fr>
2025-04-23 04:01:43 -0400

    Using the Button component instead of just a simple, JSX button

--- components/CalendlyPopText.tsx
+++ components/CalendlyPopText.tsx
@@ -1,44 +1,39 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import Button from "./ui/Button";
 
 const CalendlyPopText = () => {
   useEffect(() => {
-      const link = document.createElement('link')
-      link.href = 'https://assets.calendly.com/assets/external/widget.css';
-      link.rel = 'stylesheet';
-      document.head.appendChild(link);
+    const link = document.createElement("link");
+    link.href = "https://assets.calendly.com/assets/external/widget.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
 
-
-
-      const script = document.createElement('script');
-      script.src = 'https://assets.calendly.com/assets/external/widget.js';
-      script.async = true;
-      document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
   }, []);
 
   const openCalendly = () => {
     // @ts-ignore
-      window.Calendly.initPopupWidget({
-        url: 'https://calendly.com/hamza-ekram786/30min?background_color=d9d9d9',
-      });
-    };
+    window.Calendly.initPopupWidget({
+      url: "https://calendly.com/hamza-ekram786/30min?background_color=d9d9d9",
+    });
+  };
 
   return (
-    <button
-      onClick={openCalendly}
-      className="ml-4 px-5 py-2 bg-[#535251] text-white rounded-md hover:bg-[#161616] transition"
-    >
+    <Button onClick={openCalendly} variant="primary" className="ml-4">
       Book Now
-    </button>
+    </Button>
   );
 };
 
 export default CalendlyPopText;
 
-
-
-{/*
+{
+  /*
 
 <!-- Calendly link widget begin -->
 <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
@@ -46,5 +41,5 @@ export default CalendlyPopText;
 <a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/hamza-ekram786/30min?background_color=d9d9d9'});return false;">Book Now</a>
 <!-- Calendly link widget end --></link>
 
-*/}
-
+*/
+}
